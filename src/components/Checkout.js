eaceec199696a2887a0e84a913908050eb21cb62
UndeadAlien <workhutchc@gmail.com>
2020-11-10 15:16:20 -0600

    Fixed Checkout, and added styling and animation

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,40 +1,59 @@
 import React from 'react'
 import '../styles/Checkout.css'
-import Subtotal from '../components/Subtotal'
+
 import { useStateValue } from '../StateProvider';
+import { Link } from 'react-router-dom'
+
+import Subtotal from '../components/Subtotal'
 import CheckoutProduct from './CheckoutProduct';
+import FlipMove from 'react-flip-move';
 
 function Checkout() {
 
-    const [{ basket, user }, dispatch] = useStateValue();
+    const [{ basket, user }] = useStateValue();
 
     return (
         <div className="checkout">
             <div className="checkout__left">
-                <img
+                {<img
                     className="checkout__ad"
                     src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
                     alt=""
-                />
+                />}
 
-                <div>
-                    <h3>Hello, {user?.email}</h3>
-                    <h2 className="checkout__title">Shopping basket</h2>
-                    {basket.map(item => (
-                        <CheckoutProduct
-                            id={item.id}
-                            title={item.title}
-                            image={item.image}
-                            price={item.price}
-                            rating={item.rating}
-                        />
-                    ))}
+                {basket?.length === 0 ? (
+                    <div>
+                        <h2>Your Cart is Empty</h2>
+                        <Link to="/">
+                            <button className="empty__button">Continue Shopping</button>
+                        </Link>
+                    </div>
+                ) : (
+                        <div>
+                            <h3>Hello, {user?.email}</h3>
+                            <h2 className="checkout__title">Shopping basket</h2>
+                            <FlipMove appearAnimation="fade" enterAnimation="fade" leaveAnimation="fade">
+                                {basket.map((item, index) => (
+                                    <div key={index}>
+                                        <CheckoutProduct
+                                            id={item.id}
+                                            title={item.title}
+                                            image={item.image}
+                                            price={item.price}
+                                            rating={item.rating}
+                                        />
+                                    </div>
+                                ))}
+                            </FlipMove>
+                        </div>
+                    )}
+            </div>
+
+            {basket.length > 0 && (
+                <div className="checkout__right">
+                    <Subtotal />
                 </div>
-            </div>
-
-            <div className="checkout__right">
-                <Subtotal />
-            </div>
+            )}
         </div>
     )
 }
